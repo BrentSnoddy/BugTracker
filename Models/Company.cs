9ee7b5c2c019ef10a88c116b8b4cec0e75a5bdcd
BrentSnoddy <brentsnoddy1@gmail.com>
2022-05-23 20:12:12 -0400

    added BTRoleService and BTCompanyInfoService. plus interfaces. plus finished models.

---
+++ Models/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        [DisplayName("Company Name")]
+        public string CompanyName { get; set; }
+
+        [DisplayName("Company Description")]
+        public string Description { get; set; }
+
+        //Navigation Properties//
+        public virtual ICollection<BTUser> Members { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+
+        public virtual ICollection<Invite> Invites { get; set; }
+
+
+    }
+}
---
+++ Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,28 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Services.Interfaces
+{
+    interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+
+        public Task<List<BTUser>> GetUsersNotInRolesAsync(string roleName, int companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+
+    }
+}
